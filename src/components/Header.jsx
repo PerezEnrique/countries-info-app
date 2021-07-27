1ac6cc9e8571c7a955d66c535c06be445a3dc7bf
Enrique Perez <28enriqueperez@gmail.com>
2021-07-27 07:55:32 -0400

    feat: finish header basic structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>Countries info</h1>
+            <button>Dark mode</button>
+        </header>
+    )
+}
