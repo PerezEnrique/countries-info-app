1ffacb47f882ba8f581bdab92a681228c8172684
Enrique Perez <28enriqueperez@gmail.com>
2021-07-29 13:04:29 -0400

    fix: change font file extension and import it from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import Header from "./components/Header.jsx";
 import "./assets/styles/style.scss";
+import "./assets/styles/fonts.scss";
+import Header from "./components/Header.jsx";
 import SearchBox from "./components/SearchBox.jsx";
+import Filter from "./components/Filter";
 
-export default function App(){
-    return(
-        <React.Fragment>
-            <Header/>
-            <SearchBox/>
-        </React.Fragment>
-    )
+export default function App() {
+	return (
+		<React.Fragment>
+			<Header />
+			<SearchBox />
+			<Filter />
+		</React.Fragment>
+	);
 }
