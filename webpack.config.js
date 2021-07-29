ad7ae389c523f3803c5265cfc82811c638712806
Enrique Perez <28enriqueperez@gmail.com>
2021-07-29 13:24:29 -0400

    fix: Specify webpack mode on config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,57 @@
 const path = require("path");
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-            }
-          },
-          {
-            test: /\.html$/i,
-            loader: 'html-loader',
-          },
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                // Translates CSS into CommonJS
-                "css-loader",
-                // Compiles Sass to CSS
-                "sass-loader",
-            ],
-          },
-          {
-            test: /\.(png|svg|jpg)$/i,
-            type: 'asset/resource',
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf)$/i,
-            type: 'asset/resource',
-          },
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-          template: './public/index.html',
-          filename: './index.html',
-        }),
-        new MiniCssExtractPlugin({
-          filename: 'assets/styles/[name].css',
-        }),
-    ],
-}
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	mode: "development",
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					// Translates CSS into CommonJS
+					"css-loader",
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.(png|svg|jpg)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./public/index.html",
+			filename: "./index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "assets/styles/[name].css",
+		}),
+	],
+};
