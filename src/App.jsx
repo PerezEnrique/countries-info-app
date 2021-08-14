2b2c8d2ee0ed0ba032a3d7f5c7be9743700eca4b
Enrique Perez <28enriqueperez@gmail.com>
2021-08-14 17:55:01 -0400

    feat: Implement dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./assets/styles/style.scss";
 import "./assets/styles/fonts.scss";
 import Header from "./components/Header.jsx";
 import SearchBox from "./components/SearchBox.jsx";
 import Filter from "./components/Filter";
 import CardGrid from "./components/CardGrid";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
+	const [darkTheme, setDarkTheme] = useState(
+		localStorage.getItem("dark-theme") == "true" ? true : false
+	);
+
+	useEffect(() => {
+		localStorage.setItem("dark-theme", darkTheme);
+		console.log("aplicador", darkTheme);
+		if (darkTheme) {
+			document.documentElement.classList.add("dark-theme");
+		} else {
+			document.documentElement.classList.remove("dark-theme");
+		}
+	}, [darkTheme]);
+
 	return (
-		<React.Fragment>
-			<Header />
+		<Router>
+			<Header
+				darkTheme={darkTheme}
+				toggleTheme={() => setDarkTheme((prevTheme) => !prevTheme)}
+			/>
+			<ToastContainer />
 			<SearchBox />
 			<Filter />
 			<CardGrid />
-		</React.Fragment>
+		</Router>
 	);
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import {IconContext} from "react-icons";
-import {IoMoonOutline} from "react-icons/io5"
+import React from "react";
+import { IconContext } from "react-icons";
+import { IoMoonOutline, IoMoonSharp } from "react-icons/io5";
 
-export default function Header() {
-    return (
-        <IconContext.Provider value={{className: "header__btn__icon"}}>
-            <header className="header">
-                <h1>Countries info</h1>
-                <button className="header__btn"><IoMoonOutline/>Dark mode</button>
-            </header>
-        </IconContext.Provider>
-    )
+export default function Header({ darkTheme, toggleTheme }) {
+	return (
+		<IconContext.Provider value={{ className: "header__btn__icon" }}>
+			<header className="header">
+				<h1>Countries info</h1>
+				<button className="header__btn" onClick={toggleTheme}>
+					{!darkTheme ? <IoMoonOutline /> : <IoMoonSharp />}
+					Dark mode
+				</button>
+			</header>
+		</IconContext.Provider>
+	);
 }
