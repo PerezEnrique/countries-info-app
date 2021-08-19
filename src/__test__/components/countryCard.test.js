4b482d95b365175dd23c015025c7629305e67359
Enrique Perez <28enriqueperez@gmail.com>
2021-08-19 09:16:28 -0400

    refactor: use beforeEach to render Country card component

--- src/__test__/components/countryCard.test.js
+++ src/__test__/components/countryCard.test.js
@@ -3,23 +3,21 @@ import { render, screen } from "@testing-library/react";
 import CountryCard from "../../components/CountryCard";
 
 describe("<CountryCard/>", () => {
+	beforeEach(() => render(<CountryCard />));
+
 	it("must render a heading (to display country's name)", () => {
-		render(<CountryCard />);
 		expect(screen.getByRole("heading")).toBeInTheDocument();
 	});
 
 	it("must have the term 'population' within the description list", () => {
-		render(<CountryCard />);
 		expect(screen.getByText(/population:/i)).toBeInTheDocument();
 	});
 
 	it("must have the term 'region' within the description list", () => {
-		render(<CountryCard />);
 		expect(screen.getByText(/region:/i)).toBeInTheDocument();
 	});
 
 	it("must have the term 'capital' within the description list", () => {
-		render(<CountryCard />);
 		expect(screen.getByText(/capital:/i)).toBeInTheDocument();
 	});
 });
