5bbad1227665127c344be0f3215d81a754cfb597
Enrique Perez <28enriqueperez@gmail.com>
2021-09-02 08:15:14 -0400

    feat: Setup wepack to support routing

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,12 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
+		publicPath: "/",
 	},
 	mode: "development",
+	devServer: {
+		historyApiFallback: true,
+	},
 	resolve: {
 		extensions: [".js", ".jsx"],
 	},
