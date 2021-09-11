f9b799050d662bb0edab2a72b70d9d8cc0fd04ac
Enrique Perez <28enriqueperez@gmail.com>
2021-09-11 19:10:55 -0400

    fix: Add accessibility improvments to filter

--- src/components/common/Filter.jsx
+++ src/components/common/Filter.jsx
@@ -8,7 +8,7 @@ export default function Filter({ items, selectedItem, handleSelection, criterion
 	const [showOptions, setShowOptions] = useState(false);
 
 	const handleClick = (e) => {
-		handleSelection(e.target.dataset.value);
+		handleSelection(e.target.id);
 		setShowOptions(false);
 	};
 
@@ -18,7 +18,8 @@ export default function Filter({ items, selectedItem, handleSelection, criterion
 				className="filter__dropdown"
 				id="filter-dropdown"
 				onClick={() => setShowOptions(!showOptions)}
-				aria-expanded={!showOptions ? "false" : "true"}
+				aria-haspopup="listbox"
+				aria-expanded={!showOptions ? undefined : "true"} //passing undefined here is the same as not including it at all. This way we can create something closer to Collapsible Dropdown Listbox Example by W3.org
 			>
 				{selectedItem === "All" ? `Filter by ${criterion}` : selectedItem}
 				{!showOptions ? <RiArrowDropDownLine /> : <RiArrowDropUpLine />}
@@ -30,13 +31,18 @@ export default function Filter({ items, selectedItem, handleSelection, criterion
 						: "filter__dropdown__box filter__dropdown__box--active"
 				}
 				aria-labelledby="filter-dropdown"
+				role="listbox"
+				tabIndex="-1" //Makes the listbox focusable.
+				aria-activedescendant={selectedItem === "All" ? undefined : selectedItem}
 			>
 				{items.map((item) => (
 					<li
 						key={uuidv4()}
+						id={item}
 						className="filter__dropdown__box__option"
-						data-value={item}
 						onClick={handleClick}
+						role="option"
+						aria-selected={item !== selectedItem ? "false" : "true"}
 					>
 						{item}
 					</li>
