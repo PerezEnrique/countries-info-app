d4752f0313116f4799f0c9cb9db074b64a64e781
Enrique Perez <28enriqueperez@gmail.com>
2021-09-25 15:52:06 -0400

    Update url endpoint

--- src/services/countriesService.js
+++ src/services/countriesService.js
@@ -2,7 +2,8 @@ import http from "./httpService";
 import config from "../config.json";
 
 const apiUrl = process.env.NODE_ENV === "test" ? "" : config.apiUrl;
+const accessKey = process.env.REACT_APP_API_ACCESS_KEY;
 
 export function getCountries() {
-	return http.get(`${apiUrl}/all`);
+	return http.get(`${apiUrl}/all?access_key=${accessKey}`);
 }
