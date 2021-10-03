f5e15b01626fdb692e127fe3b2f09d15dcbd07b5
Enrique Perez <28enriqueperez@gmail.com>
2021-10-03 11:44:25 -0400

    refactor: Separate router related logic from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { CountriesProvider } from "./contexts/CountriesContext";
 import { getThemeStatus } from "./services/themeService";
-import Header from "./components/Header.jsx";
-import AllCountries from "./pages/AllCountries";
-import Country from "./pages/Country";
 import "./assets/styles/style.scss";
 import "./assets/styles/style-desktop.scss";
 import "./assets/styles/fonts.scss";
+import AppRouter from "./AppRouter";
+import Header from "./components/Header.jsx";
 
 export default function App() {
 	const [darkTheme, setDarkTheme] = useState(getThemeStatus() == "true" ? true : false);
@@ -21,17 +19,12 @@ export default function App() {
 	}, [darkTheme]);
 
 	return (
-		<Router>
-			<CountriesProvider>
-				<Header
-					darkTheme={darkTheme}
-					toggleTheme={() => setDarkTheme((prevTheme) => !prevTheme)}
-				/>
-				<Switch>
-					<Route path="/country/:code" component={Country} />
-					<Route exact path="/" component={AllCountries} />
-				</Switch>
-			</CountriesProvider>
-		</Router>
+		<CountriesProvider>
+			<Header
+				darkTheme={darkTheme}
+				toggleTheme={() => setDarkTheme((prevTheme) => !prevTheme)}
+			/>
+			<AppRouter />
+		</CountriesProvider>
 	);
 }
