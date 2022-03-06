480897acc572ed2a7b9dfab2baab254fbe3f2229
Enrique Perez <28enriqueperez@gmail.com>
2022-03-06 10:45:08 -0400

    feat: Implement webpack-bundle-analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,25 @@ const common = require("./webpack.common");
 const TerserPlugin = require("terser-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const shouldAnalyze = process.argv.includes("--analyze");
+//Remeber: Since we are using npm and not yarn, in order to pass the additional flag we need npm run build -- --analyze
+
+const plugins = [
+	new CopyPlugin({
+		patterns: [
+			{
+				from: path.resolve(__dirname, "public", "_redirects"),
+				to: "./",
+			},
+		],
+	})
+];
+
+if(shouldAnalyze) {
+	plugins.push(new BundleAnalyzerPlugin());
+}
 
 module.exports = merge(common, {
 	mode: "production",
@@ -11,14 +30,5 @@ module.exports = merge(common, {
 		minimize: true,
 		minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
 	},
-	plugins: [
-		new CopyPlugin({
-			patterns: [
-				{
-					from: path.resolve(__dirname, "public", "_redirects"),
-					to: "./",
-				},
-			],
-		}),
-	],
+	plugins
 });
